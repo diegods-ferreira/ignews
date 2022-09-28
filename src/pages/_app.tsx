46557a18fdc709212acc944a756bc6b42ee52f31
Diego Ferreira <diegosf1705@gmail.com>
2022-09-28 08:11:07 -0300

    Autenticação com GitHub com Next Auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,21 @@
 import { AppProps } from 'next/app';
+import { Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
 
 import { Header } from '../components/Header';
 
 import '../styles/global.scss';
 
-function MyApp({ Component, pageProps }: AppProps) {
+type MyAppProps = AppProps<{
+  session: Session;
+}>;
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }: MyAppProps) {
   return (
-    <>
+    <SessionProvider session={session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
 
