6f181b7ff24dfedf2e651dbbad42de817fc12eee
Diego Ferreira <diegosf1705@gmail.com>
2022-10-09 16:04:44 -0300

    Página: Posts

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+
+import styles from './styles.module.scss';
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <time>12 de março de 2022</time>
+            <strong>Creating a Monorepo with Lorena & Yarn Workspaces</strong>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat maxime eaque dolorum nihil. Corporis sapiente mollitia sunt odit inventore non ipsam sed cum magni. Assumenda, tenetur! Perspiciatis cumque esse doloribus!</p>
+          </a>
+
+          <a href="#">
+            <time>12 de março de 2022</time>
+            <strong>Creating a Monorepo with Lorena & Yarn Workspaces</strong>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat maxime eaque dolorum nihil. Corporis sapiente mollitia sunt odit inventore non ipsam sed cum magni. Assumenda, tenetur! Perspiciatis cumque esse doloribus!</p>
+          </a>
+
+          <a href="#">
+            <time>12 de março de 2022</time>
+            <strong>Creating a Monorepo with Lorena & Yarn Workspaces</strong>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat maxime eaque dolorum nihil. Corporis sapiente mollitia sunt odit inventore non ipsam sed cum magni. Assumenda, tenetur! Perspiciatis cumque esse doloribus!</p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
